fadddd4b61203ee6cd0084432b98109b196294c5
SUDHANSHU UNIYAL <uniyal1001@gmail.com>
2020-07-21 23:08:50 +0530

    Update router.js
    final commit

--- router.js
+++ router.js
@@ -6,7 +6,7 @@ const cors = require("cors")
 
 apiRouter.use(cors())
 
-apiRouter.get("/", (req, res) => res.json("Hello, if you see this message that means your backend is up and running successfully. Congrats! Now let's continue learning React!"))
+apiRouter.get("/", (req, res) => res.json("blog-management running..."))
 
 // check token to log out front-end if expired
 apiRouter.post("/checkToken", userController.checkToken)
