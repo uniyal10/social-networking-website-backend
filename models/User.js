486fd3f62598a519c6f162a3d658a7d47c6b792b
uniyal10 <uniyal1001@gmail.com>
2022-02-14 00:19:48 +0530

    changes to site

--- models/User.js
+++ models/User.js
@@ -47,7 +47,7 @@ User.prototype.validate = function () {
     if (this.data.password == "") {
       this.errors.push("You must provide a password.")
     }
-    if (this.data.password.length > 0 && this.data.password.length < 12) {
+    if (this.data.password.length > 0 && this.data.password.length < 8) {
       this.errors.push("Password must be at least 12 characters.")
     }
     if (this.data.password.length > 50) {
